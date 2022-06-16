4d8d8148935bd3f26a35285e5ab89076afe6d81d
kh-desktop <kenghwee.t@gmail.com>
2022-06-16 23:43:30 +0800

    Create trades type

---
+++ server/src/Trades/typeDef.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server'
+
+const typeDef = gql`
+  type Trade {
+    id: Int!
+    ticker: String!
+    quantity: Float!
+    action: String!
+    type: String!
+    openDate: DateTime!
+    openFee: Float!
+    strikePrice: Float
+    expirationDate: DateTime
+    closeDate: DateTime
+    closePrice: Float
+    closeFee: Float
+    transaction: Float
+  }
+`
