d4351ad0273c1135563f900a0cc931f1d79ba19f
kh-desktop <kenghwee.t@gmail.com>
2022-06-20 23:13:46 +0800

    Update ApiError

--- server/src/Utilities/typeDef.ts
+++ server/src/Utilities/typeDef.ts
@@ -1,9 +1,20 @@
 import { gql } from 'apollo-server'
 
+export class ApiError {
+  code: string
+  message: string
+
+  constructor(code: string, message: string) {
+    this.code = code
+    this.message = message
+  }
+}
+
 export const typeDef = gql`
   scalar Date
 
   type ApiError {
+    code: String!
     message: String!
   }
 `
